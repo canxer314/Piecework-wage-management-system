511f23e477b65d31ca0f1e00121602d1c74975dd
canxer314 <canxer314@live.com>
2017-04-11 19:37:49 +0800

    add Employee class.

---
+++ Piecework-wage-management-system/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piecework_wage_management_system
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public short EmployeeID { get; set; }
+        public string Password { get; set; }
+        public string Department { get; set; }
+        public string Workshop { get; set; }
+        public string Job { get; set; }
+        public byte Authority { get; set; }
+        public string Telephone { get; set; }
+    }
+}
